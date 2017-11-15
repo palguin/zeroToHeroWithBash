2550a5c99c1cd8689b785a2dfc45e73f3bdbeb72
Pablo Alguindigue <Lito@loop-depaulsecure-252-83.depaulsecure-student.depaul.edu>
2017-11-14 18:03:04 -0600

    finished a bash practice assingment

--- practice4.sh
+++ practice4.sh
@@ -1,19 +1,40 @@
+#!/bin/bash
+
 echo exercise 1:
 echo
 
-echo renaming jpg files to include date
-echo
-
 sleep 1
-if [ -f *.jpg ]
-then
-	for i in *.jpg
-	do 
-		mv *.jpg $(date +"%m-%d-%y")${i}
-	done
+if [ $# -gt 0  ]; then
+	echo renaming jpg files to include date
+	echo
+	if [ -f *.jpg ]; then
+		for i in *.jpg
+		do 
+			mv *.jpg $(date +"%m-%d-%y")${i}
+		done
+	else
+		echo No jpg file exists
+	fi
 else
-	echo No jpg file exists
+	echo continuing to part 2
 fi
 
 echo 
 echo exercise 2:
+echo
+
+#Needs testing
+read -p "Enter a file extension: " EXTENSION
+read -p "Enter a file prefix: " PREFIX
+
+if [[ $PREFIX == "" ]]; then
+        PREFIX=$(date +"%y-%m-%d")
+fi
+
+
+for f in $(ls *.$EXTENSION 2>/dev/null) ; do
+	if [ -f $f ]; then 
+		echo "$f -> ${PREFIX}${f}"
+		mv $f ${PREFIX}${f}
+	fi
+done
